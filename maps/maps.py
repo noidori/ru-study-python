003bb84668f91545d613f1f83d57d9b679bc9b8e
noidori <idrisov201@gmail.com>
2023-02-02 09:34:21 +0300

    Resolved maps exercise

--- maps/maps.py
+++ maps/maps.py
@@ -4,28 +4,21 @@ from typing import Union
 class MapExercise:
     @staticmethod
     def rating(list_of_movies: list[dict]) -> float:
-        """
-        !!Задание нужно решить используя map!!
-        Посчитать средний рейтинг фильмов (rating_kinopoisk) у которых две или больше стран.
-        Фильмы у которых рейтинг не задан или равен 0 не учитывать в расчете среднего.
-
-        :param list_of_movies: Список фильмов.
-        Ключи словаря: name, rating_kinopoisk, rating_imdb, genres, year, access_level, country
-        :return: Средний рейтинг фильмов у которых две или больше стран
-        """
-        pass
+        rating_list = []
+        for key in list_of_movies:
+            if "," in key["country"] and key["rating_kinopoisk"] not in ("", "0"):
+                rating_list.append(key.get("rating_kinopoisk"))
+                rating_list = list(map(float, rating_list))
+        return sum(rating_list) / len(rating_list)
 
     @staticmethod
     def chars_count(list_of_movies: list[dict], rating: Union[float, int]) -> int:
-        """
-        !!Задание нужно решить используя map!!
-        Посчитать количество букв 'и' в названиях всех фильмов с рейтингом (rating_kinopoisk) больше
-        или равным заданному значению
+        def count_char(key: dict) -> int:
+            if key["rating_kinopoisk"] not in "" and float(key["rating_kinopoisk"]) >= rating:
+                return str(key["name"]).count("и")
 
-        :param list_of_movies: Список фильмов
-        Ключи словаря: name, rating_kinopoisk, rating_imdb, genres, year, access_level, country
-        :param rating: Заданный рейтинг
-        :return: Количество букв 'и' в названиях всех фильмов с рейтингом больше
-        или равным заданному значению
-        """
-        pass
+            return 0
+
+        result = sum(map(count_char, list_of_movies))
+
+        return result
