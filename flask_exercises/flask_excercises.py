b201b37b297d6a0764218e3da89feed191e68e87
noidori <idrisov201@gmail.com>
2023-02-15 17:40:40 +0300

    modified the solution to flask_excercises.py in accordance with the comments to the PR

--- flask_exercises/flask_excercises.py
+++ flask_exercises/flask_excercises.py
@@ -8,25 +8,31 @@ class FlaskExercise:
     def configure_routes(app: Flask) -> None:
         db: dict = {}
 
-        @app.route("/user", methods=["POST"])
+        @app.post("/user")
         def new_user() -> Any:
             username = request.json.get("name")
-            if username:
-                db[username] = username
-                return {"data": f"User {username} is created!"}, 201
-            return {"errors": {"name": "This field is required"}}, 422
+            if not username:
+                return {"errors": {"name": "This field is required"}}, 422
+            db[username] = username
+            return {"data": f"User {db[username]} is created!"}, 201
 
-        @app.route("/user/<name>", methods=["GET", "PATCH", "DELETE"])
-        def modify_user(name: str) -> Any:
-            if request.method == "GET":
-                if db.get(name):
-                    return {"data": f"My name is {db[name]}"}, 200
+        @app.get("/user/<name>")
+        def find_user(name: str) -> Any:
+            if db.get(name):
+                return {"data": f"My name is {db[name]}"}, 200
+            return "User not found", 404
+
+        @app.patch("/user/<name>")
+        def update_user(name: str) -> Any:
+            new_name = request.json.get("name")
+            if not new_name:
                 return "User not found", 404
-            if request.method == "PATCH":
-                new_name = request.json.get("name")
-                db[name] = new_name
-                return {"data": f"My name is {new_name}"}, 200
-            if request.method == "DELETE":
-                db.pop(name)
-                return "User was deleted", 204
-            return "", 500
+            db[name] = new_name
+            return {"data": f"My name is {db[name]}"}, 200
+
+        @app.delete("/user/<name>")
+        def delete_user(name: str) -> Any:
+            if name not in db.keys():
+                return "User not found", 404
+            db.pop(name)
+            return "User was deleted", 204
